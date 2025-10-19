827d66ca48c14e019eb6e95b4deef66f4a1e716c
robertIT1A <robertoguevarrajr25@gmail.com>
2025-10-19 16:15:33 +0800

    Add files via upload

---
+++ guess_game.py
@@ -0,0 +1,22 @@
+# pick a random number
+
+
+import random
+dice = random.randint(1,6)
+print(dice)
+
+tries = 3
+
+while tries != 0:
+    pick = int(input("\nEnter your guess number: "))
+    tries -= 1
+    if pick == dice:
+        print("You're guess is correct!")
+        print(f"Only took {tries} tries")
+        break
+
+    else:
+        print("Wrong Guess!")
+        print(f"Remaining Tries is {tries}")
+
+print("Out of try")
